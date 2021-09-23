1c3effdc6661aa88d36dbf15e8d732e477137517
System Administrator <root@aguns-MacBook-Air.local>
2021-09-23 18:13:55 +0700

    Component wrapped

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react"
 import { getDatabase, ref, get, child } from 'firebase/database'
-import { InferGetServerSidePropsType } from "next"
 
 const dbRef = ref(getDatabase())
 
@@ -12,21 +11,18 @@ interface PortfolioSnapshot {
 
 const PortfolioFetching = () => (
     <div className="grid grid-cols-2 gap-3">
-        {
-            [...Array(4)].map((item, index) => (
-                <div className="border border-yellow-200 border-opacity-25 shadow rounded-lg p-4 w-full mx-auto" key={index}>
-                    <div className="animate-pulse flex space-x-4">
+        {[...Array(4)].map((index) => (
+            <div className="border border-yellow-200 border-opacity-25 shadow rounded-lg p-4 w-full mx-auto" key={index}>
+                <div className="animate-pulse flex space-x-4">
                     <div className="flex-1 space-y-4 py-1">
                         <div className="h-4 bg-yellow-200 opacity-25 rounded w-full"></div>
                         <div className="space-y-2">
-                        <div className="h-2 bg-gray-600 rounded w-5/6"></div>
+                            <div className="h-2 bg-gray-600 rounded w-5/6"></div>
                         </div>
                     </div>
-                    </div>
                 </div>
-            ))
-        }
-        
+            </div>
+        ))}
     </div>
 )
 
@@ -47,16 +43,12 @@ const Portfolio = () => {
 
     return (
         <div className="grid grid-cols-2 gap-3">
-            {
-                portfolio?.map((item, index) => (
-                    <a key={index} href={item.link} target="_blank" rel="noreferrer" className="p-4 rounded-lg border-yellow-200 border">
-                        <h2 className="font-bold text-yellow-200">{item.name}</h2>
-                        <h2 className="font-semibold text-gray-400 text-sm">{item.year}</h2>
-                    </a>
-                ))
-            }
-            
-
+            {portfolio?.map((item, index) => (
+                <a key={index} href={item.link} target="_blank" rel="noreferrer" className="p-4 rounded-lg border-yellow-200 border">
+                    <h2 className="font-bold text-yellow-200">{item.name}</h2>
+                    <h2 className="font-semibold text-gray-400 text-sm">{item.year}</h2>
+                </a>
+            ))}
         </div>
     )
 }
--- pages/read/[post].tsx
+++ pages/read/[post].tsx
@@ -8,26 +8,24 @@ import style from '../../styles/index.module.sass'
 import Head from 'next/head'
 import { formatDate } from "../../lib/helpers"
 
-const PostFetching = () => {
-  return (
-    <div className="animate-pulse flex space-x-4">
-      <div className="flex-1 space-y-2 py-1">
-        <div className="h-6 bg-yellow-200 opacity-25 rounded w-full"></div>
-        <div className="h-4 bg-yellow-200 opacity-25 rounded w-3/4"></div>
-        <div className="space-y-2 pt-6">
-          <div className="h-2 bg-gray-600 rounded"></div>
-          <div className="h-2 bg-gray-600 rounded w-5/6"></div>
-          <div className="h-2 bg-yellow-200 opacity-25 rounded"></div>
-          <div className="h-2 bg-gray-600 rounded w-5/6"></div>
-          <div className="h-2 bg-gray-600 rounded"></div>
-          <div className="h-2 bg-yellow-200 opacity-25 rounded w-5/6"></div>
-          <div className="h-2 bg-gray-600 rounded"></div>
-          <div className="h-2 bg-gray-600 rounded w-5/6"></div>
-        </div>
+const PostFetching = () => (
+  <div className="animate-pulse flex space-x-4">
+    <div className="flex-1 space-y-2 py-1">
+      <div className="h-6 bg-yellow-200 opacity-25 rounded w-full"></div>
+      <div className="h-4 bg-yellow-200 opacity-25 rounded w-3/4"></div>
+      <div className="space-y-2 pt-6">
+        <div className="h-2 bg-gray-600 rounded"></div>
+        <div className="h-2 bg-gray-600 rounded w-5/6"></div>
+        <div className="h-2 bg-yellow-200 opacity-25 rounded"></div>
+        <div className="h-2 bg-gray-600 rounded w-5/6"></div>
+        <div className="h-2 bg-gray-600 rounded"></div>
+        <div className="h-2 bg-yellow-200 opacity-25 rounded w-5/6"></div>
+        <div className="h-2 bg-gray-600 rounded"></div>
+        <div className="h-2 bg-gray-600 rounded w-5/6"></div>
       </div>
     </div>
-  )
-}
+  </div>
+)
 
 const Post: NextPage = () => {
   const router = useRouter()
@@ -42,20 +40,24 @@ const Post: NextPage = () => {
       })
     }
   }, [router.query])
+
+  if (fetching) {
+    return (
+      <div className={style.wrapper}>
+        <PostFetching/>
+      </div>
+    )
+  }
   
   return (
     <div className={style.wrapper}>
       <Head>
         <title>{post?.title ?? 'Loading...'}</title>
       </Head>
-      {fetching && <PostFetching/>}
-      {! fetching && (
-        <>
-          <h1 className="text-4xl font-extrabold text-yellow-200 bh-font">{post?.title}</h1>
-          <span className="text-gray-300 text-sm">{post?.author.displayName}, {formatDate(''+post?.published)}</span>
-          <div className="mt-6 text-gray-50 article" dangerouslySetInnerHTML={{__html: ''+post?.content}}></div>
-        </>
-      )}
+
+      <h1 className="text-4xl font-extrabold text-yellow-200 bh-font">{post?.title}</h1>
+      <span className="text-gray-300 text-sm">{post?.author.displayName}, {formatDate(''+post?.published)}</span>
+      <div className="mt-6 text-gray-50 article" dangerouslySetInnerHTML={{__html: ''+post?.content}}></div>
     </div>
   )
 }
