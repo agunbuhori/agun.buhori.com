2b0a1f10524e2e945118fcd90d95cfcf4fe44100
agunbuhori <agun.buhori@gmail.com>
2021-09-26 13:16:47 +0700

    isMounted added

--- lib/isMounted.ts
+++ lib/isMounted.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react"
 
-export default function isMounted(): { current: boolean } {
+export default function UseIsMounted(): { current: boolean } {
     const componentIsMounted = useRef(true)
     useEffect(() => {
       return () => { componentIsMounted.current = false }
