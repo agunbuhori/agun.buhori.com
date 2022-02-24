ab88efaab6a8cd1a291e23880ce8494a92f37650
Agun Buhori <agun.buhori@gmail.com>
2022-02-24 15:04:08 +0700

    Update useFetch.ts

--- lib/useFetch.ts
+++ lib/useFetch.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const http = axios.create({
     baseURL: "https://www.googleapis.com/blogger/v3/blogs/1874723781606441837/",
     params: {
-        'key': 'AIzaSyBkYgF6vpuVgj7Qa3Vk2AOZFP5no7MwJOM'
+        'key': 'AIzaSyBDnsdAbwICO_6BvDKyU3Xqm1k3VpL4RRE'
     }
 })
